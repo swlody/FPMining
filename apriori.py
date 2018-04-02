bf64b2753205ca87ef3dffa4967b92a4be47083a
Sam Wlody <SWlody@gmail.com>
2018-04-01 23:08:20 -0400

    General cleanup

--- apriori.py
+++ apriori.py
@@ -1,22 +1,40 @@
+from __future__ import print_function
 from itertools import combinations
-import csv
-import argparse
+from argparse import ArgumentParser
+from csv import reader
 
-labels = ['age', 'workclass', 'fnlwgt', 'education', 'education-num', 'marital-status', 'occupation', 'relationship', 'race', 'sex', 'capital-gain', 'capital-loss', 'hours-per-week', 'native-country', 'probability']
 
-def gen_transactions_from_csv(file):
+def main():
+    parser = init_parser()
+    args = parser.parse_args()
+
+    database = csv_to_transaction_db(args.filename)
+    freq_itemsets = apriori(database, args.min_sup)
+
+    for j in range(len(freq_itemsets)):
+        print("Frequent", j + 1, "itemsets:", freq_itemsets[j])
+        if j != len(freq_itemsets) - 1:
+            print()
+
+
+def csv_to_transaction_db(filename):
     """ Produce a list of transactions, where each transaction is a set of tuples
     containing the label and value of the item. In theory, this should be the only
     method that needs to be reimplemented for simple datasets. """
-    with open(file, newline='') as csvfile:
+    with open(filename) as file:
         database = []
-        for row in csv.reader(csvfile, delimiter=','):
+        # Ignore the probability label - I don't know what it's used for
+        labels = ['age', 'workclass', 'fnlwgt', 'education', 'education-num', 'marital-status', 'occupation',
+                  'relationship', 'race', 'sex', 'capital-gain', 'capital-loss', 'hours-per-week', 'native-country']
+        for row in reader(file, delimiter=','):
             # Create a tuple of the label and the value of each item in the row
             # this will ensure different hashes in the case of values
-            # from different columns coincidentally being equal
-            database.append({(labels[i], row[i].strip()) for i in range(len(labels) - 1)})
-            # length - 1 to ignore probability
+            # from different columns coincidentally being equal.
+            # Each transaction in the database is a set of such tuples.
+            transaction = {(labels[i], row[i].strip()) for i in range(len(labels))}
+            database.append(transaction)
         return database
+
 
 def join(l1, l2):
     """ Join two itemsets that have all items equal except the last """
@@ -24,56 +42,64 @@ def join(l1, l2):
     copy.append(l2[-1])
     return copy
 
+
 def gen_freq_1_itemsets(database, min_sup):
     """ Get all items in the database that meet the minimum support """
-    count = dict()
+    count = {}
     for transaction in database:
         for item in transaction:
+            # Use default of 0 for count.get() if item is not already in dict
             count[item] = count.get(item, 0) + 1
     return [[k] for k, v in count.items() if v >= min_sup]
 
-def has_infrequent_subset(c, L, k):
-    return all(list(s) not in L for s in combinations(c, k-1))
 
-def gen_candidates(L, k):
+def has_infrequent_subset(candidate, itemsets, k):
+    return all(list(s) not in itemsets for s in combinations(candidate, k - 1))
+
+
+def candidates(itemsets, k):
     """ Apriori method to generate candidate itemsets """
-    length = k-2
-    for l1 in L:
-        for l2 in L:
+    length = k - 2
+    for l1 in itemsets:
+        for l2 in itemsets:
+            # If the itemsets are the same except for the last item
             if all(l1[i] == l2[i] for i in range(length)) and l1[length] < l2[length]:
-                c = join(l1, l2)
-                if not has_infrequent_subset(c, L, k):
-                    yield c
+                candidate = join(l1, l2)
+                if not has_infrequent_subset(candidate, itemsets, k):
+                    yield candidate
 
-def apriori(D, min_sup):
-    # Convert from relative to absolute support
-    min_sup *= len(D)
-    L = [gen_freq_1_itemsets(D, min_sup)]
+
+def apriori(database, min_sup):
+    # Convert from relative (ratio of occurences) to absolute (number of occurences) support
+    min_sup *= len(database)
+    itemsets_list = [gen_freq_1_itemsets(database, min_sup)]
     k = 2
-    while L[k-2]:
-        L.append([])
+    while itemsets_list[k-2]:
+        itemsets_list.append([])
         # For each potential candidate
-        for c in gen_candidates(L[k-2], k):
+        for candidate in candidates(itemsets_list[k-2], k):
             count = 0
             # Count the number of times it appears in the database
-            for t in D:
-                if all(item in t for item in c):
+            for transaction in database:
+                if all(item in transaction for item in candidate):
                     count += 1
             # If candidate meets the minimum support, add it to the list of frequent-k itemsets
             if count >= min_sup:
-                L[k-1].append(c)
+                itemsets_list[k-1].append(candidate)
         k += 1
-    return L
+    return itemsets_list
+
+
+def init_parser():
+    parser = ArgumentParser(description="Run Apriori Algorithm on a given transaction database.")
+    parser.add_argument('filename', nargs='?', default='adult.data', help="Path to CSV file with transaction database.")
+    parser.add_argument('--min_sup', type=float, nargs='?', default=0.5,
+                        help="Minimum support an itemset must meet to avoid being pruned.")
+    return parser
+
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Run Apriori Algorithm on a given transaction database.")
-    parser.add_argument('file', nargs='?', default='adult.data', help="Path to CSV file with transaction database.")
-    parser.add_argument('--min_sup', type=float, nargs='?', default=0.5, help="Minimum support an itemset must meet to avoid being pruned.")
-    args = parser.parse_args()
-
-    transactions = gen_transactions_from_csv(args.file)
-    freq_itemsets = apriori(transactions, args.min_sup)
-
-    for i in range(len(freq_itemsets)):
-        print("Frequent", i+1 ,"itemset:", len(freq_itemsets[i]) if freq_itemsets[i] else 0)
-        print()
+    try:
+        main()
+    except KeyboardInterrupt:
+        exit(0)
